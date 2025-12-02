4b88bd288089439ed1ab5f4f00069b24cede5b5a
Nilov Vladimir <nilov.v04@yandex.ru>
2025-12-02 14:37:21 +0300

    Swap func places

--- calculator.py
+++ calculator.py
@@ -1,9 +1,9 @@
 import math
 
-def get_area(a, b):
+def get_hypotenuse(a, b):
  return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 
-def get_hypotenuse(a, b):
+def get_area(a, b):
  return a * b
 
 if __name__ == "__main__":
